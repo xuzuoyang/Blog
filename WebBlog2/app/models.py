54badb8984f86198995b0ccfd9f7008ee7f3ba26
xuzuoyang <xuzuoyang@daokoudai.com>
2017-06-22 23:28:09 +0800

    add pagedown conversion;post permalink&edit function

--- WebBlog2/app/models.py
+++ WebBlog2/app/models.py
@@ -1,5 +1,7 @@
 import hashlib
 import forgery_py
+import bleach
+from markdown import markdown
 from random import seed, randint
 from sqlalchemy.exc import IntegrityError
 from datetime import datetime
@@ -178,6 +180,7 @@ class Post(db.Model):
 	__tablename__ = 'posts'
 	id = db.Column(db.Integer, primary_key=True)
 	body = db.Column(db.Text)
+	body_html = db.Column(db.Text)
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -192,3 +195,14 @@ class Post(db.Model):
 					author=u)
 			db.session.add(p)
 			db.session.commit()
+
+	@staticmethod
+	def on_changed_body(target, value, oldvalue, initiator):
+		allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+						'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+						'h1', 'h2', 'h3', 'p']
+		target.body_html = bleach.linkify(bleach.clean(
+			markdown(value, output_format='html'),
+			tags=allowed_tags, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
